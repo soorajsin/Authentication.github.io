c8e7dd040ad8a91625caba79f2d98e5807c3da1c
Sooraj Singh <115391013+soorajsin@users.noreply.github.com>
2023-08-18 11:16:26 +0530

    Update connection.js

--- server/DB/connection.js
+++ server/DB/connection.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 
 
 
-const db="mongodb+srv://soorajsingh7505:sooraj231@crud-app.4oebebt.mongodb.net/?retryWrites=true&w=majority";
+const db="database_url";
 
 
 mongoose.connect(db, {
